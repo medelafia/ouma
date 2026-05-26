58a05ffebf75370ea268e3754418f5181a3aa35f
MOHAMED EL AFIA <mohamedelafia723@gmail.com>
2026-05-26 13:03:45 +0100

    fix uses

--- setup_everything_on_kubernetes.sh
+++ setup_everything_on_kubernetes.sh
@@ -37,30 +37,24 @@ if command -v docker &> /dev/null && command -v helm &> /dev/null && command -v 
         helm install influxdb influxdata/influxdb2 --set adminUser.existingSecret=influxdb-auth --namespace monitoring
         echo "📈✅ Influx service created successfully"
     fi 
-    # appending the IPs of the related services to env file
-    head -n $(($(wc -l < .env)-3)) .env > temp.env && mv temp.env .env
-    echo "📝 Appending IPs addresses to the .env file ..."
-    echo "TARGET_SERVER_HOST=$(kubectl get svc prometheus-kube-prometheus-prometheus --namespace monitoring | grep prometheus-kube-prometheus-prometheus | awk '{print $3}')" >> .env
-    echo "MYSQL_HOST=$(kubectl get svc mysql-service --namespace monitoring | grep mysql-service | awk '{print $3}')" >> .env
-    echo "INFLUX_DB_URL=$(kubectl get svc influxdb-influxdb2 --namespace monitoring | grep influxdb-influxdb2 | awk '{print $3}')" >> .env
-    echo "📝✅ IPs Appended successfully"
     ## build backend and frontend images 
+    echo "⚒️⛏️ Building images"
     docker build -t ouma:latest .
     docker build -t ouma-ui:latest ./ouma-ui
-
-    #docker tag 
-
+    echo "⚒️⛏️✅ Build comleted"
     ## Setup ouma backend
-    echo "Setting up application backend ..."
+    echo "🆙 Startup Ouma backend ..."
     kubectl create secret generic ouma-secret --from-env-file=.env --namespace monitoring
     kubectl apply -f k8s/ouma/deployment.yaml
     kubectl apply -f k8s/ouma/service.yaml
+    echo "✅ Ouma backend ready ..."
     ## Setup ouma frontend 
-    echo "Setting up application frontend ..."
+    echo "🆙 Startup Ouma frontend ..."
     kubectl create secret generic ouma-ui-secret --from-env-file=./ouma-ui/.env --namespace monitoring
     kubectl apply -f k8s/ouma-ui/deployment.yaml
     kubectl apply -f k8s/ouma-ui/service.yaml
-    echo "✅ Done"
+    echo "✅ Ouma frontend ready ..."
+    echo "✅✅✅ Done"
 else 
     echo "❌ Before starting the setup, ensure that Docker, Kubernetes, and Helm are installed on your system.";
 fi 
